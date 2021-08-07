973580c822166f8abde8d937ebf96d949d37828d
sukanto-m <48436741+sukanto-m@users.noreply.github.com>
2021-08-07 16:58:00 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -9,16 +9,23 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace, Datastore, Dataset
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+
+
+ds = TabularDatasetFactory.from_delimited_files(path=path)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 ### YOUR CODE HERE ###a
 
